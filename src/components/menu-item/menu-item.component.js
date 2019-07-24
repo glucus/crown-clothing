d1ba90c7caa24381cea3de120026e4cd286a8d70
glucus <voskresenskayaas@gmail.com>
2019-07-24 16:08:57 +0300

    edited menu-item style (added background zoom on hover)

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -3,14 +3,13 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{
-      backgroundImage: `url(${imageUrl})`
-    }}>
+    <div className={`${size} menu-item`}>
+      <div className='menu-item-background' style={{
+        backgroundImage: `url(${imageUrl})`
+      }}/>
       <div className='content'>
-        <h1 className='title'>{title}</h1>
-        <span className='subtitle'>Shop now</span>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>
   );
