a03dab63e0de234ca0dfd2a080a39236edf0d64c
glucus <voskresenskayaas@gmail.com>
2019-07-24 13:41:41 +0300

    edited directory component

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component'
 
+import { sections } from './directory.data'
 import './directory.styles.scss'
-
-const menuItems = [
-  {id: 0, title: 'Hats', displaySize: 'small'},
-  {id: 1, title: 'Jackets', displaySize: 'small'},
-  {id: 2, title: 'Sneakers', displaySize: 'small'},
-  {id: 3, title: 'Womens', displaySize: 'large'},
-  {id: 4, title: 'Mens', displaySize: 'large'},
-];
 
 class Directory extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      sections: sections
+    };
   }
 
   render() {
+
+    const {sections} = this.state;
+
     return (
       <div className='directory-menu'>
-        {menuItems && menuItems.map(
-          item => <MenuItem key={item.id} title={item.title}/>
+        {sections.map(
+          ({title, imageUrl, id}) => <MenuItem key={id}
+                                               title={title}
+                                               imageUrl={imageUrl}
+          />
         )}
       </div>
     );
