ed0fb1e0e752c42e5f387a5a6236871fd9c49d25
glucus <voskresenskayaas@gmail.com>
2019-07-28 19:32:32 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+};
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Homepage/>
+        <Switch>
+          <Route exact path="/" component={Homepage}/>
+          <Route path="/hats" component={HatsPage}/>
+        </Switch>
       </BrowserRouter>
     );
   }
