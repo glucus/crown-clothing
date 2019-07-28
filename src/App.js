16234d9b214edadab2003267220e9870b335975a
glucus <voskresenskayaas@gmail.com>
2019-07-28 13:33:56 +0300

    added browser-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Homepage />
+      <BrowserRouter>
+        <Homepage/>
+      </BrowserRouter>
     );
   }
 }
