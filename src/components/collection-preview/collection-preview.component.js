27e11b474f79d1d1797dc7a790a3ccb346511713
glucus <voskresenskayaas@gmail.com>
2019-09-15 21:09:04 +0300

    edited  collections-preview component

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => {
+
+  const previewItems = items.slice(0, 4);
+
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.map(
+        {previewItems.map(
           item => <div key={item.id} className=''>
             {item.name}
           </div>
